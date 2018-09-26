3fef507d6a16f04a19f3d83ad1a94703b47bf641
Jini Byun <jinibyun@gmail.com>
2018-09-26 00:04:29 -0400

    Commit

--- Csharp/ConsoleApp/Program.cs
+++ Csharp/ConsoleApp/Program.cs
@@ -10,7 +10,34 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            // basic
+            // 1. DataType
+            Console.WriteLine("=================== Data Type ================");
+            var dt = new DataType();
+            dt.Test();
 
+            // 2. Variable And Constant
+            Console.WriteLine("=================== Variable & Constant ================");
+            var vnc = new VariableAndConstant();
+            vnc.Test();
+
+            // 3. Array
+            Console.WriteLine("=================== Array ================");
+            var arr = new ArrayTest();
+            arr.Test();
+
+            // 4. String
+            Console.WriteLine("=================== String ================");
+            var strTest = new StringTest();
+            strTest.Test();
+
+            // 5. Enum
+            Console.WriteLine("=================== Enum ================");
+            var enumTest = new EnumTest();
+            enumTest.Test();
+
+            // 6. Operator
+            
         }
     }
 }
