a2353ca30ce6cdb2ce01f4da00ce6193ddaa65ca
Jini Byun <sungmin_byun@manulife.com>
2020-02-12 22:00:02 -0500

    commit

---
+++ JavaScript/001HelloWorld.js
@@ -0,0 +1,12 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+    function greetMe(yourName) {
+      //alert('Hello ' + yourName);
+    }
+    
+    greetMe('World');
+
+    console.log("dsfdsfdsf");
+    /* End of your code */
+  })();
