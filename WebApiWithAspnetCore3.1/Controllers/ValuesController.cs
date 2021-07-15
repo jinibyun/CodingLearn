134f54927ffb1d63b6f57f49de903c07e092164d
Jini Byun <jinibyun@gmail.com>
2021-07-15 18:08:38 -0400

    WebApi

--- WebApiWithAspnetCore3.1/Controllers/ValuesController.cs
+++ WebApiWithAspnetCore3.1/Controllers/ValuesController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiWithAspnetCore31.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
